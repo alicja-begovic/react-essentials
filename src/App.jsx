af1be8dab6394f83b1a867c1eca5be1365475bb5
Alicja Begović <alicja.begovic@gmail.com>
2023-12-13 00:13:02 +0100

    finish project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { Fragment } from "react";
-
-import Header from "./components/Header/Header";
-import CoreConcepts from "./components/CoreConcepts";
+import Header from "./components/Header/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
 
 function App() {
   return (
-    <Fragment>
+    <>
       <Header />
       <main>
         <CoreConcepts />
+        <Examples />
       </main>
-    </Fragment>
+    </>
   );
 }
 
